3b697625552475690b6b186108d881b210920473
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2023-03-06 07:31:40 +0100

    Added batch normalization

--- models.py
+++ models.py
@@ -13,22 +13,32 @@ class SurvModel(nn.Module):
         self.events = data[events_col].values
         self.time = data[time_col].values
 
-        self.fc1 = nn.Linear(len(self.x[0]), 128)
-        self.fc2 = nn.Linear(128, 64)
         self.dropout = nn.Dropout(0.1)
-        self.fc3 = nn.Linear(64, 32)
-        self.fc4 = nn.Linear(32, 1)
+
+        self.fc1 = nn.Linear(len(self.x[0]), 64)
+        self.bn1 = nn.BatchNorm1d(64)
+
+        self.fc2 = nn.Linear(64, 32)
+        self.bn2 = nn.BatchNorm1d(32)
+        
+        self.fc3 = nn.Linear(32, 16)
+        
+        self.bn3 = nn.BatchNorm1d(16)
+        self.fc4 = nn.Linear(16, 1)
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.dropout(x)
         x = F.relu(x)
+        x = self.bn1(x)
         x = self.fc2(x)
         x = self.dropout(x)
         x = F.relu(x)
+        x = self.bn2(x)
         x = self.fc3(x)
         x = self.dropout(x)
         x = F.relu(x)
+        x = self.bn3(x)
         x = self.fc4(x)
         return x
 
